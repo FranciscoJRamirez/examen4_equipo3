71e36e6af4144790e785ccbf761b044d89a0d958
FranciscoJRamirez <42820012+FranciscoJRamirez@users.noreply.github.com>
2020-12-03 15:28:52 -0700

    probando recyclevieew

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import abc
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.popup import Popup
 
 class Vehiculo(abc.ABC):
     def __init__(self, placa, marca, color):
@@ -41,3 +47,69 @@ class Coche(Vehiculo):
         else:
             self.esta_encendido = True
 
+Builder.load_string('''
+#:kivy 1.10.0
+#: import Popup kivy.uix.popup
+
+<MessageBox>:
+    title: 'Popup Message Box'
+    size_hint: None, None
+    size: 400, 400
+
+    BoxLayout:
+        orientation: 'vertical'
+        Label:
+            text: root.message
+        Button:
+            size_hint: 1, 0.2
+            text: 'OK'
+            on_press: root.dismiss()
+
+<RecycleViewRow>:
+    orientation: 'horizontal'
+    Label:
+        text: root.text
+    Button:
+        text: 'Mostrar'
+        on_press: app.root.message_box(root.text)
+
+<MainScreen>:
+    viewclass: 'RecycleViewRow'
+    RecycleBoxLayout:
+        default_size: None, dp(56)
+        default_size_hint: 1, None
+        size_hint_y: None
+        height: self.minimum_height
+        orientation: 'vertical'
+                    ''')
+
+class MessageBox(Popup):
+    message = StringProperty()
+
+class RecycleViewRow(BoxLayout):
+    text = StringProperty()
+
+class MainScreen(RecycleView):
+    lista_v=[]
+    lista_v.append(Camion("426-EE-9", "Honda", "Blanco"))
+    lista_v.append(Camion("EMN-71-71", "Mercedes benz", "Negro"))
+    lista_v.append(Coche("HAW-162-A", "Nissan", "Rojo"))
+    lista_v.append(Coche("EF-16-199", "BMW", "Plateado"))
+    def init(self, kwargs):
+        super(MainScreen, self).init(kwargs)
+        self.data = [{'text': "Button " + str(x.placa), 'id': str(x.index)} for x in (lista_v)]
+
+    def message_box(self, message):
+        p = MessageBox()
+        p.message = message
+        p.open() 
+        print('test press: ', message)
+
+class TestApp(App):
+    title = "RecycleView Direct Test"
+
+    def build(self):
+        return MainScreen()
+
+if name == "main":
+    TestApp().run()
